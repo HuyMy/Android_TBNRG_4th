f4309da6c5d92d64de960814a051e20460bb3b7b
HuyMy <lqhuy91@gmail.com>
2023-01-25 01:01:58 +0700

    Use Data Binding for Crime Detail Fragment.

--- CriminalIntent/app/src/main/java/com/huylq/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/huylq/android/criminalintent/CrimeFragment.kt
@@ -1,20 +1,17 @@
 package com.huylq.android.criminalintent
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.huylq.android.criminalintent.databinding.FragmentCrimeBinding
 import java.util.*
 
 private const val TAG = "CrimeFragment"
@@ -22,9 +19,7 @@ private const val TAG = "CrimeFragment"
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
-    private lateinit var titleField: EditText
-    private lateinit var dateButton: Button
-    private lateinit var solvedCheckBox: CheckBox
+    private lateinit var binding: FragmentCrimeBinding
     private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
         ViewModelProvider(this)[CrimeDetailViewModel::class.java]
     }
@@ -42,13 +37,14 @@ class CrimeFragment : Fragment() {
         val safeArgs: CrimeFragmentArgs by navArgs()
         val crimeId = safeArgs.crimeId as UUID
         crimeDetailViewModel.loadCrime(crimeId)
+        binding = DataBindingUtil.inflate<FragmentCrimeBinding?>(inflater, R.layout.fragment_crime, container, false)
+            .apply {
+                lifecycleOwner = viewLifecycleOwner
+                viewModel = crimeDetailViewModel
+                view = this@CrimeFragment
+            }
 
-        val view = inflater.inflate(R.layout.fragment_crime, container, false)
-        titleField = view.findViewById(R.id.crime_title) as EditText
-        dateButton = view.findViewById(R.id.crime_date) as Button
-        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
-
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -83,48 +79,20 @@ class CrimeFragment : Fragment() {
         })
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        val titleWatcher = object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                crime.title = p0.toString()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-        }
-        titleField.addTextChangedListener(titleWatcher)
-
-        solvedCheckBox.apply {
-            setOnCheckedChangeListener { _, isChecked ->
-                crime.isSolved = isChecked
-            }
-        }
-
-        dateButton.setOnClickListener {
-            val action = CrimeFragmentDirections.actionOpenDatePicker(crime.date)
-            findNavController().navigate(action)
-        }
-    }
-
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
     }
 
     private fun updateUI() {
-        titleField.setText(crime.title)
-        dateButton.text = crime.getFormattedDate()
-        solvedCheckBox.apply {
-            isChecked = crime.isSolved
-            jumpDrawablesToCurrentState()
+        binding.apply {
+            crimeDate.text = crime.getFormattedDate()
+            crimeSolved.jumpDrawablesToCurrentState()
         }
+    }
+
+    fun onDateButtonClicked() {
+        val action = CrimeFragmentDirections.actionOpenDatePicker(crime.date)
+        findNavController().navigate(action)
     }
 }
